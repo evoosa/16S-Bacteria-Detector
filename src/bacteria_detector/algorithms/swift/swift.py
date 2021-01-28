55ba031a1241f22b06de46c38dceab2751afa7a7
liorse <segev.lior@gmail.com>
2021-01-28 15:49:11 +0200

    added run_cmd for swift

--- src/bacteria_detector/algorithms/swift/swift.py
+++ src/bacteria_detector/algorithms/swift/swift.py
@@ -5,5 +5,5 @@ import os
 def get_run_cmd(sample_name: str):
     """ Get the command needed to run swift's algorithm on the given sample data """
     output_dir = os.path.join(config.DEFAULT_OUTPUT_DIR, sample_name)
-    return f'mkdir {output_dir}; ./q2wkflow_v2.sh ./config/config.txt {os.path.join(config.DEFAULT_INPUT_DIR, sample_name)} {output_dir}'
+    return f'mkdir -p {output_dir}; ./q2wkflow_v2.sh ./config/config.txt {os.path.join(config.DEFAULT_INPUT_DIR, sample_name)} {output_dir}'
 print(get_run_cmd('dddd'))
--- src/bacteria_detector/main.py
+++ src/bacteria_detector/main.py
@@ -1,4 +1,4 @@
 def main():
-    # if not eists, create output dir
+    # if not exists, create output dir
     # get list of all dirs in results dir
     pass
