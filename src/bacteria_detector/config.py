2d907e80add192202bb499e71e6188a64495fa1b
liorse <segev.lior@gmail.com>
2021-01-28 16:24:21 +0200

    added main. need to run on linux and on clustetr

--- src/bacteria_detector/config.py
+++ src/bacteria_detector/config.py
@@ -9,7 +9,6 @@ now = datetime.datetime.now().strftime("%m_%d_%Y-%H_%M")
 output_dir = DEFAULT_BASE_OUTPUT_DIR + f'_{now}'
 
 # Cluster Properties
-DEFAULT_QUEUE = 'public-himem'
+DEFAULT_QUEUE = 'gromacs'  # 'public-himem'
 DEFAULT_JOB_CPU = '1'
 DEFAULT_JOB_MEM = '16000'
-
