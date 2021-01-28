b61fabfbf501f75e456e03e4520fa52db350f91d
liorse <segev.lior@gmail.com>
2021-01-28 15:38:52 +0200

    added run_cmd for swift

--- src/bacteria_detector/utils.py
+++ src/bacteria_detector/utils.py
@@ -11,8 +11,8 @@ def get_run_algorithm_cmd(sample_name: str, algorithm: str) -> str:
         return ''
 
 
-def get_run_on_cluster_cmd(sample_name: str,
-                           algorithm: str) -> str:  # FIXME - output errors to a specific dir, even log it in future
+def get_run_on_cluster_cmd(sample_name: str, algorithm: str) -> str:
+    # FIXME - output errors to a specific dir, even log it in future
     """ Get the bsub command to run on the cluster """
     run_alg_cmd = 'CMD_TEMP'  # FIXME - get command for running the algorithm
     return f'bsub -q {config.DEFAULT_QUEUE} -n {config.DEFAULT_JOB_CPU} -R "rusage[mem={config.DEFAULT_JOB_MEM}]" -J {sample_name} -o {sample_name}.out -e {sample_name}.err {run_alg_cmd}'
