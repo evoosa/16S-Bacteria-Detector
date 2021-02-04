ee308e6f99a2694d095c108e90251fdfca748708
liorse <segev.lior@gmail.com>
2021-02-04 12:10:02 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='bacteria_detector',
+    name='bacteria-detector',
     version='1',
     packages=[''],
     url='',
-    license='Weizman Institute',
+    license='Weizmann Institute',
     author='evoosa',
     author_email='evaosher@gmail.com',
     description='16S RRNA sequences bacteria frequency detector'
