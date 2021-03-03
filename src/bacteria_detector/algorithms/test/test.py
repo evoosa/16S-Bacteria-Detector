ddc4a237d752f98a1bf801fc5eba86465a65982b
evoosa <eva@mypart.com>
2021-03-03 11:52:58 +0200

    removed test

--- src/bacteria_detector/algorithms/test/test.py
+++
@@ -1,9 +0,0 @@
-from bacteria_detector import config
-import os
-
-
-# TODO - queue and ram is different for some runs?
-def get_run_cmd(sample_name: str):
-    """ Get the command needed to run a test command on the given sample data """
-    output_file = os.path.join(config.OUTPUT_DIR, sample_name, "test", sample_name + "_test")
-    return f'touch {output_file}'
